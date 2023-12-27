c4dcbaa7acbafae109ca3f966d364b16b461af45
MaybeMonad <dezineleo@gmail.com>
2023-12-27 18:22:37 +0800

    update: add iife export

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,24 @@ import { resolve } from "path"
 import { defineConfig } from "vite"
 import dts from "vite-plugin-dts"
 
+const pkgName = "vgg-wasm"
+
+const fileName = {
+  es: `${pkgName}.mjs`,
+  cjs: `${pkgName}.cjs`,
+  iife: `${pkgName}.iife.js`,
+}
+
+const formats = Object.keys(fileName) as Array<keyof typeof fileName>
+
 export default defineConfig({
-  // optimizeDeps: {
-  //   exclude: ["submodules/**"],
-  // },
-  // server: {
-  //   fs: {
-  //     deny: ["submodules/**"],
-  //   },
-  // },
-  // esbuild: {
-  //   exclude: ["submodules/**"],
-  // },
   build: {
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
-      name: "vgg-wasm",
-      fileName: "vgg-wasm",
+      name: "vggWasm",
+      // @ts-expect-error
+      fileName: (format) => fileName[format],
+      formats,
     },
   },
   plugins: [dts({ rollupTypes: true })],
